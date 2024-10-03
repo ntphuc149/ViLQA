b75e6053e8a28cd9a92cca164e0a9d401e0ef7cf
Truong-Phuc Nguyen <99306941+ntphuc149@users.noreply.github.com>
2024-10-03 17:09:33 +0700

    Delete EQA directory

--- EQA/models/eqa_model.py
+++
@@ -1,6 +0,0 @@
-from transformers import AutoModelForQuestionAnswering, AutoTokenizer
-
-def load_model_and_tokenizer(model_checkpoint):
-    tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
-    model = AutoModelForQuestionAnswering.from_pretrained(model_checkpoint)
-    return model, tokenizer
